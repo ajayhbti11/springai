03833a2fdeed7617f9c2bd5ed6f1e6acbc93da20
Ajay RajPut <ajayhbti11@gmail.com>
2026-07-30 12:53:14 +0530

    initial setup
    initial setup

---
+++ src/main/java/com/springai/openai/controller/OpenAIController.java
@@ -0,0 +1,25 @@
+package com.springai.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OpenAIController {
+   private final ChatClient chatClient;
+
+    public OpenAIController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message){
+       return chatClient.prompt(message).call().content();
+       // return "Hello "+message;
+    }
+
+}
