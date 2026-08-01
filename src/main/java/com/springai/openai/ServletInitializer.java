308f3039890e746023d4d3e62aab1b5c4bc996ea
Ajay RajPut <ajayhbti11@gmail.com>
2026-08-02 01:14:34 +0530

    Spring open AI run
    can run open ai model

--- src/main/java/com/springai/openai/ServletInitializer.java
+++ src/main/java/com/springai/openai/ServletInitializer.java
@@ -1,13 +1,13 @@
-package com.springai.openai;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(OpenaiApplication.class);
-	}
-
-}
+//package com.springai.openai;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(OpenaiApplication.class);
+//	}
+//
+//}
