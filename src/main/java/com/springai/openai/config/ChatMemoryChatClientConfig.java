7d1668ff096c9e28590a840a7f542db2fb207511
Ajay RajPut <ajayhbti11@gmail.com>
2026-08-15 00:30:32 +0530

    chat memory client
    chat memory client

---
+++ src/main/java/com/springai/openai/config/ChatMemoryChatClientConfig.java
@@ -0,0 +1,36 @@
+package com.springai.openai.config;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatMemoryChatClientConfig {
+
+//    @Bean
+//    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+//        return MessageWindowChatMemory.builder().maxMessages(10)
+//                .chatMemoryRepository(jdbcChatMemoryRepository).build();
+//    }
+
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(OpenAiChatModel openAiChatModel, ChatMemory chatMemory) {
+
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return ChatClient.builder(openAiChatModel)
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
+                .build();
+
+    }
+
+}
